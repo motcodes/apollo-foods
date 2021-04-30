ef5457a702e9777543f9f03e03a19b9073aabc4e
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-04-30 23:11:01 +0200

    added font

---
+++ fonts/fonts.js
@@ -0,0 +1,13 @@
+// import { createGlobalStyle } from 'styled-components'
+
+// import Blatant from './Blatant.woff2'
+
+// export default createGlobalStyle`
+//     @font-face {
+//         font-family: 'Blatant';
+//         src: local('Blatant'),
+//         url(${Blatant}) format('woff2');
+//         font-weight: 400;
+//         font-style: normal;
+//     }
+// `
