1185cd52564b4063418e9e9c00949bdca7e33f85
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-05-04 20:49:34 +0200

    added local font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,16 @@
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../components/theme/globalStyles'
-// import GlobalFonts from '../fonts/fonts'
 import { themeStyles } from '../components/theme/theme'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      {/* <GlobalFonts /> */}
       <ThemeProvider theme={themeStyles}>
         <Head>
           <title>Apollo Foods 🚀</title>
+          <link href="./font.css" rel="stylesheet" />
         </Head>
         <Component {...pageProps} />
       </ThemeProvider>
