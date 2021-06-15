0cba6f9eb5e04ababe4b8c5a2ff34eb5a3406c2c
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-06-15 17:43:07 +0200

    added comment

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,8 +1,3 @@
-/*
-Author: Matthias Oberholzer
-Multimedia Project 1 - Web
-Salzburg University of Applied Sciences
-*/
 import styled from 'styled-components'
 
 const Layout = (props) => (
--- components/pouch.jsx
+++ components/pouch.jsx
@@ -1,8 +1,3 @@
-/*
-Author: Matthias Oberholzer
-Multimedia Project 1 - Web
-Salzburg University of Applied Sciences
-*/
 import { useRef } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 
