6e2a0bdad4e269b57fb362e4d74cdc3f370e6b5d
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-06-17 15:35:07 +0200

    added create recipe page

---
+++ pages/create.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import Layout from '../components/Layout'
+import { Typography } from '../utils'
+
+export default function Create() {
+  return (
+    <Layout>
+      <Typography variant="h1">Create</Typography>
+    </Layout>
+  )
+}
